3f7c69e787be4189deab0e002adb4dfaa1aa9427
Juri Berlanda <juriberlanda@hotmail.com>
2020-12-30 15:43:39 +0100

    [ABL] Set NOZZLE_TO_PROBE_OFFSET.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- marlin/Configurations/config/examples/Creality/Ender-3 Pro/BigTreeTech SKR Mini E3 2.0/Configuration.h
+++ marlin/Configurations/config/examples/Creality/Ender-3 Pro/BigTreeTech SKR Mini E3 2.0/Configuration.h
@@ -982,7 +982,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -5, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
