bed1a51b8ae779d2740d6c6026f3eab5cb8dcad7
Juri Berlanda <juriberlanda@hotmail.com>
2021-05-24 14:19:01 +0200

    [Version] Set version to v2.4.0.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- marlin/Configurations/config/examples/Creality/Ender-3 Pro/BigTreeTech SKR Mini E3 2.0/Configuration.h
+++ marlin/Configurations/config/examples/Creality/Ender-3 Pro/BigTreeTech SKR Mini E3 2.0/Configuration.h
@@ -140,7 +140,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 Pro v2.4-dev (Voron M4)"
+#define CUSTOM_MACHINE_NAME "Ender-3 Pro v2.4.0 (Voron M4)"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
